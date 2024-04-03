5b2333f5c2d27e0dd10bdd4e27c5d1b03cb43c94
seaniiio <cnc4750@naver.com>
2024-04-04 00:49:57 +0900

    feat: Member 모델 생성

--- django_advanced/member/models.py
+++ django_advanced/member/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    is_leader = models.BooleanField(default=False)
+    hearts = models.PositiveIntegerField(default=0)
