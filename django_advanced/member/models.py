73d942c4ffd266b7bea70439a4c783e8f8679bd1
gayoung <gayoung2037@gmail.com>
2024-04-05 17:45:19 +0900

    6,7 update

--- django_advanced/member/models.py
+++ django_advanced/member/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Member(models.Model):
+    name = models.CharField(max_length=50, default="member")
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=50)
     is_leader = models.BooleanField(default=False)
